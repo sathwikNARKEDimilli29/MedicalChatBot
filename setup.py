35549c0b8451ebeaf38f48dda5bc66e026c1ecbc
sathwik narkedimilli <sathwik.narkedimilli29@gmail.com>
2025-08-26 12:03:26 +0530

    requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="sathwik narkedimilli",
+    author_email="sathwik.narkedimilli@ieee.org",
+    packages=find_packages(),
+    install_requires=[],
+)
